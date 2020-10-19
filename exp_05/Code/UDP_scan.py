f64c89cf3e185d925229885e3190498a21264574
MN962464 <3042680194@qq.com>
2020-10-19 21:59:06 +0800

    实验完成提交

---
+++ exp_05/Code/UDP_scan.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+import matplotlib 
+matplotlib.use('Agg') 
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.layers.inet import IP,UDP, TCP, ICMP,IP_PROTOS
+from scapy.all import sr,sr1,RandShort
+
+dst_ip = "172.16.111.135"
+src_port = RandShort()
+dst_port=53
+dst_timeout=10
+
+def udp_scan(dst_ip,dst_port,dst_timeout):
+    udp_scan_resp = sr1(IP(dst=dst_ip)/UDP(dport=dst_port),timeout=dst_timeout)
+    if (str(type(udp_scan_resp))=="<class 'NoneType'>"):
+        retrans = []
+        for count in range(0,3):
+            retrans.append(sr1(IP(dst=dst_ip)/UDP(dport=dst_port),timeout=dst_timeout))
+        for item in retrans:
+            if (str(type(item))!="<class 'NoneType'>"):
+                udp_scan(dst_ip,dst_port,dst_timeout)
+        return "Open|Filtered"
+    elif (udp_scan_resp.haslayer(UDP) or udp_scan_resp.getlayer(IP).proto == IP_PROTOS.udp):
+        return "Open"
+    elif(udp_scan_resp.haslayer(ICMP)):
+        if(int(udp_scan_resp.getlayer(ICMP).type)==3 and int(udp_scan_resp.getlayer(ICMP).code)==3):
+            return "Closed"
+        elif(int(udp_scan_resp.getlayer(ICMP).type)==3 and int(udp_scan_resp.getlayer(ICMP).code) in [1,2,9,10,13]):
+            return "Filtered"
+
+if __name__ == '__main__':
+    result = udp_scan(dst_ip,dst_port,dst_timeout)
+    print (result)
